9a1bc9c471d4f300f1589ad9eeaebbda112baf6a
beckysanchez <rebeca.carrillosn@uanl.edu.mx>
2025-10-19 20:17:22 -0600

    Add start script for Render

--- server.js
+++ server.js
@@ -164,9 +164,9 @@ app.post('/agregar-amigo', (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3006;
-
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
 });
+
 
