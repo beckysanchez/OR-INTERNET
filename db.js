7302928288bcad781fe663f57ca6d2b9971140d7
beckysanchez <rebeca.carrillosn@uanl.edu.mx>
2025-10-19 22:35:46 -0600

    fix db connection railway

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'tu-host-de-railway.proxy.rlwy.net',
-  user: 'root',
-  password: 'auVsnsFRweRTUBAdcoikMbcZbySCvAUs',
-  database: 'railway',
-  port: 11186
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
+
 
 db.connect((err) => {
   if (err) {
