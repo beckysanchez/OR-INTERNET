41f3a1e90edbdae0839a22e9153eda55043f057f
beckysanchez <rebeca.carrillosn@uanl.edu.mx>
2025-11-22 23:16:46 -0600

    migracion a xamp incompleta

--- db.js
+++ db.js
@@ -1,19 +1,18 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  host: 'localhost',
+  user: 'root',
+  password: 'Monse171002',
+  database: 'pwci',
+  port: 3306
 });
-
 
 db.connect((err) => {
   if (err) {
     console.error('❌ Error al conectar a MySQL:', err);
   } else {
-    console.log('✅ Conectado a MySQL en Railway');
+    console.log('✅ Conectado a MySQL en XAMPP (localhost)');
   }
 });
 
